e92c3e0f094724408ff9a7a55638fd131d645d60
Venelin Efremov <ghent360@iqury.us>
2020-10-04 16:18:02 -0700

    Re affange the FreeRTOSConfig.h file to match what is generated from MX_Cube for easy comparison.

--- FreeRTOSConfig/FreeRTOSConfig.h
+++ FreeRTOSConfig/FreeRTOSConfig.h
@@ -54,41 +54,42 @@
  * See http://www.freertos.org/a00110.html.
  *----------------------------------------------------------*/
 
-extern uint32_t SystemCoreClock;
-
-#define configSUPPORT_STATIC_ALLOCATION			1
-#if __LPC17xx__
-    #define configSUPPORT_DYNAMIC_ALLOCATION        1
-#else
-    #define configSUPPORT_DYNAMIC_ALLOCATION        0
+/* Ensure definitions are only used by the compiler, and not by the assembler. */
+#if defined(__ICCARM__) || defined(__CC_ARM) || defined(__GNUC__)
+  #include <stdint.h>
+  extern uint32_t SystemCoreClock;
 #endif
-#define configUSE_PREEMPTION					1
-#define configUSE_PORT_OPTIMISED_TASK_SELECTION	1
-#define configUSE_QUEUE_SETS					1
-#define configUSE_IDLE_HOOK						0
-#define configUSE_TICK_HOOK						1
-#define configCPU_CLOCK_HZ						( SystemCoreClock )
-#define configTICK_RATE_HZ						( 1000 )
-#define configMAX_PRIORITIES					( 6 )
-#define configMINIMAL_STACK_SIZE				( ( unsigned short ) 120 )
 
-#define configAPPLICATION_ALLOCATED_HEAP        ( 0 )
-
+#define configUSE_PREEMPTION                     1
+#define configSUPPORT_STATIC_ALLOCATION          1
+#if __LPC17xx__
+    #define configSUPPORT_DYNAMIC_ALLOCATION     1
+#else
+    #define configSUPPORT_DYNAMIC_ALLOCATION     0
+#endif
+#define configUSE_QUEUE_SETS                     1
+#define configUSE_IDLE_HOOK                      0
+#define configUSE_TICK_HOOK                      1
+#define configCPU_CLOCK_HZ                       ( SystemCoreClock )
+#define configTICK_RATE_HZ                       ((TickType_t)1000)
+#define configMAX_PRIORITIES                     ( 6 )
+#define configMINIMAL_STACK_SIZE                 ((uint16_t)120)
 //SD:: now using Heap5 
-//#define configTOTAL_HEAP_SIZE                    ( ( size_t ) ( 1024) )
-
-#define configMAX_TASK_NAME_LEN					( 10 )
-#define configUSE_TRACE_FACILITY				1
-#define configUSE_16_BIT_TICKS					0
-#define configIDLE_SHOULD_YIELD					1
-#define configUSE_MUTEXES						1
-#define configQUEUE_REGISTRY_SIZE				8
-#define configCHECK_FOR_STACK_OVERFLOW			2
-#define configUSE_RECURSIVE_MUTEXES				1
-#define configUSE_MALLOC_FAILED_HOOK			1
-#define configUSE_APPLICATION_TASK_TAG			0
-#define configUSE_COUNTING_SEMAPHORES			1
-#define configUSE_NEWLIB_REENTRANT 				0	// We'd like to use 1 but that makes the tasks too big because strint _reent is so large
+//#define configTOTAL_HEAP_SIZE                  ((size_t)1024))
+#define configMAX_TASK_NAME_LEN                  ( 16 )
+#define configUSE_TRACE_FACILITY                 1
+#define configUSE_16_BIT_TICKS                   0
+#define configUSE_MUTEXES                        1
+#define configQUEUE_REGISTRY_SIZE                8
+#define configCHECK_FOR_STACK_OVERFLOW           2
+#define configUSE_RECURSIVE_MUTEXES              1
+#define configUSE_MALLOC_FAILED_HOOK             1
+#define configUSE_COUNTING_SEMAPHORES            1
+#define configUSE_PORT_OPTIMISED_TASK_SELECTION  1
+#define configAPPLICATION_ALLOCATED_HEAP         ( 0 )
+#define configIDLE_SHOULD_YIELD                  1
+#define configUSE_APPLICATION_TASK_TAG           0
+#define configUSE_NEWLIB_REENTRANT               0 // We'd like to use 1 but that makes the tasks too big because strint _reent is so large
 
 /* The full demo always has tasks to run so the tick will never be turned off.
 The blinky demo will use the default tickless idle implementation to turn the
@@ -109,63 +110,61 @@ FreeRTOS/Source/tasks.c for limitations. */
 #define configUSE_STATS_FORMATTING_FUNCTIONS	0
 
 /* Co-routine definitions. */
-#define configUSE_CO_ROUTINES 			0
-#define configMAX_CO_ROUTINE_PRIORITIES ( 2 )
+#define configUSE_CO_ROUTINES                    0
+#define configMAX_CO_ROUTINE_PRIORITIES          ( 2 )
 
 /* Software timer definitions. */
-#define configUSE_TIMERS				0
-#define configTIMER_TASK_PRIORITY		( 2 )
-#define configTIMER_QUEUE_LENGTH		5
+#define configUSE_TIMERS                         0
+#define configTIMER_TASK_PRIORITY                ( 2 )
+#define configTIMER_QUEUE_LENGTH                 5
 #if __LPC17xx__  || defined(TARGET_STM32F4)
 // TODO::: vTaskList says using 32words.... configTIMER_TASK_STACK_DEPTH currently set to 120words
-    #define configTIMER_TASK_STACK_DEPTH	( /*50*/ 120 )
+  #define configTIMER_TASK_STACK_DEPTH           ( /*50*/ 120 )
 #else
-    #define configTIMER_TASK_STACK_DEPTH    ( configMINIMAL_STACK_SIZE )
+  #define configTIMER_TASK_STACK_DEPTH           ( configMINIMAL_STACK_SIZE )
 
 #endif
 
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
-#define INCLUDE_vTaskPrioritySet		1
-#define INCLUDE_uxTaskPriorityGet		1
-#define INCLUDE_vTaskDelete				1
-#define INCLUDE_vTaskCleanUpResources	0
-#define INCLUDE_vTaskSuspend			1
-#define INCLUDE_vTaskDelayUntil			1
-#define INCLUDE_vTaskDelay				1
-#define INCLUDE_eTaskGetState			1
-#define INCLUDE_xTimerPendFunctionCall	0
-#define INCLUDE_uxTaskGetStackHighWaterMark	1
-#define INCLUDE_xQueueGetMutexHolder	1
-#define INCLUDE_xTaskGetSchedulerState    1
+#define INCLUDE_vTaskPrioritySet             1
+#define INCLUDE_uxTaskPriorityGet            1
+#define INCLUDE_vTaskDelete                  1
+#define INCLUDE_vTaskCleanUpResources        0
+#define INCLUDE_vTaskSuspend                 1
+#define INCLUDE_vTaskDelayUntil              1
+#define INCLUDE_vTaskDelay                   1
+#define INCLUDE_xTaskGetSchedulerState       1
+#define INCLUDE_xTimerPendFunctionCall       0
+#define INCLUDE_xQueueGetMutexHolder         1
+#define INCLUDE_uxTaskGetStackHighWaterMark  1
+#define INCLUDE_eTaskGetState                1
 
 /* Cortex-M specific definitions. */
 #ifdef __NVIC_PRIO_BITS
 	/* __BVIC_PRIO_BITS will be specified when CMSIS is being used. */
-	#define configPRIO_BITS       		__NVIC_PRIO_BITS
+ #define configPRIO_BITS         __NVIC_PRIO_BITS
 #else
-    #if __LPC17xx__ 
-        #define configPRIO_BITS               5        /* 32 priority levels */
-    #else
-        #define configPRIO_BITS               4        /* 15 priority levels */
-    #endif
+ #if __LPC17xx__ 
+   #define configPRIO_BITS       5        /* 32 priority levels */
+ #else
+   #define configPRIO_BITS       4        /* 15 priority levels */
+ #endif
 #endif
 
 /* The lowest interrupt priority that can be used in a call to a "set priority"
 function. */
 #if __LPC17xx__
-    #define configLIBRARY_LOWEST_INTERRUPT_PRIORITY            31
+ #define configLIBRARY_LOWEST_INTERRUPT_PRIORITY   31
 #else
-    #define configLIBRARY_LOWEST_INTERRUPT_PRIORITY			0xf
+ #define configLIBRARY_LOWEST_INTERRUPT_PRIORITY   15
 #endif
 
 /* The highest interrupt priority that can be used by any interrupt service
 routine that makes calls to interrupt safe FreeRTOS API functions.  DO NOT CALL
 INTERRUPT SAFE FREERTOS API FUNCTIONS FROM ANY INTERRUPT THAT HAS A HIGHER
 PRIORITY THAN THIS! (higher priorities are lower numeric values. */
-#define configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY	5
-
-
+#define configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY 5
 
 /* Interrupt priorities used by the kernel port layer itself.  These are generic
 to all Cortex-M ports, and do not rely on any particular library functions. */
@@ -181,27 +180,28 @@ extern void vAssertCalled( uint32_t ulLine, const char *pcFile ) NOEXCEPT;
 
 /* Definitions that map the FreeRTOS port interrupt handlers to their CMSIS
 standard names. */
+#define vPortSVCHandler    SVC_Handler
 #define xPortPendSVHandler PendSV_Handler
-#define vPortSVCHandler SVC_Handler
-#define xPortSysTickHandler SysTick_Handler
 
 /* The priority used by the Ethernet MAC driver interrupt. */
-#define configMAC_INTERRUPT_PRIORITY	( configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY )
+#define configMAC_INTERRUPT_PRIORITY          ( configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY )
 
 /* Dimensions a buffer that can be used by the FreeRTOS+CLI command
 interpreter.  See the FreeRTOS+CLI documentation for more information:
 http://www.FreeRTOS.org/FreeRTOS-Plus/FreeRTOS_Plus_CLI/ */
-#define configCOMMAND_INT_MAX_OUTPUT_SIZE		1024
+#define configCOMMAND_INT_MAX_OUTPUT_SIZE     1024
 
 /* If configINCLUDE_DEMO_DEBUG_STATS is set to one, then a few basic IP trace
 macros are defined to gather some UDP stack statistics that can then be viewed
 through the CLI interface. */
-#define configINCLUDE_DEMO_DEBUG_STATS 0
+#define configINCLUDE_DEMO_DEBUG_STATS        0
+
+#define xPortSysTickHandler SysTick_Handler
 
 
 #ifdef LPC_DEBUG
-    //debugging
-    #define configINCLUDE_FREERTOS_TASK_C_ADDITIONS_H     1
+ //debugging
+ #define configINCLUDE_FREERTOS_TASK_C_ADDITIONS_H     1
 #endif
 
 #endif /* FREERTOS_CONFIG_H */
